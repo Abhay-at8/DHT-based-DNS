8c1f355d5a3fdff4ceca72b0f6fdbb9bab6803ec
Abhay <abhaytripathi98.com>
2024-02-16 22:00:27 +0530

    added files

--- setEnv.sh
+++ setEnv.sh
@@ -15,3 +15,15 @@ mkdir -p /home/rp/
 cd /home/rp/
 git clone https://github.com/Abhay-at8/DNS_RP.git
 chmod -R 777  /home/rp/
+
+
+#cloud-boothook
+#!/bin/bash
+apt update -y
+apt install openjdk-17-jre-headless -y
+mkdir -p /home/rp/
+cd /home/rp/
+git clone https://github.com/Abhay-at8/DNS_RP.git
+chmod -R 777  /home/rp/
+cd DNS_RP
+bash connectChord.sh 3.238.148.111
